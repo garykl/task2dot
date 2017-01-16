ad964236f516df1d09f2345cc8b5ad1046027217
garykl <spiehr@gmx.de>
2017-01-16 20:03:35 +0100

    remove unused code

--- taskwarrior2net/validate.py
+++ taskwarrior2net/validate.py
@@ -1,49 +1,6 @@
 import textwrap
 
 
-def excludeElementsFrom(toExclude, ll):
-    """
-    toExclude and ll are lists of elements.
-    return a new list, containing all elements of ll,
-    which are not in toExclude.
-    """
-    res = []
-    for l in ll:
-        if not l in toExclude:
-            res.append(l)
-    return res
-
-def isTimed(task):
-    """
-    return time for task that is timed via task id start/stop.
-    time is zero when it is not stopped.
-    """
-    def timeFromStamps(start, stop):
-        if start[:8] != stop[:8]:
-            return 0
-        else:
-            return int(stop[9:15]) - int(start[9:15])
-
-    starts = []
-    stops = []
-    time = 0
-    if 'annotations' in task:
-        for a in task['annotations']:
-            if 'Started task' in a['description']:
-                starts.append(a['entry'])
-            if 'Stopped task' in a['description']:
-                stops.append(a['entry'])
-    for (start, stop) in zip(starts, stops):
-        time += timeFromStamps(start, stop)
-
-
-class Annotation(object):
-    def __init__(self, entry, description):
-        self.entry = entry
-        self.description = description
-
-
-
 def task_with_annotations(task):
 
     def wrap_text(strng, chars_per_line=25):
@@ -95,16 +52,4 @@ class TaskwarriorExploit(object):
         res = {}
         for task in self.tasks:
             res[task['uuid']] = task
-        return res
-
-    def annotations(self):
-        res = []
-        for task in self.tasks:
-            if 'annotations' in task and task['status'] is not 'deleted':
-                for a in task['annotations']:
-                    if 'Started task' in a['description']:
-                        continue
-                    if 'Stopped task' in a['description']:
-                        continue
-                    res.append(Annotation(a['entry'], a['description']))
         return res
